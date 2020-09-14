74c4b26d6b64a1475180107185c7174657c2f892
Caue Obici <caueobici@gmail.com>
2020-09-14 18:33:30 -0400

    Upload to git

---
+++ README.md
@@ -0,0 +1,6 @@
+# SKINS SEARCH DISCORD BOT
+
+It uses web-scrapping to get information about the skins prices and some APIs to convert currencies.
+
+NOTE: idk if it keeps working, it was an old project. 
+If you want to try it, you should generate an discord OAuth token and get an access key to use http://data.fixer.io API (currency convertor).
--- currencies.js
+++ currencies.js
@@ -3,7 +3,9 @@ const rp = require('request-promise');
 const cheer = require('cheerio');
 const time = 1000 * 60 * 60; //1 Hour
 
-const link = 'http://data.fixer.io/api/latest?access_key=0a6d7c738d5297e0840407d28f6c7b97&format=1';
+const access_key = "ACCESS_KEY"
+
+const link = 'http://data.fixer.io/api/latest?access_key=' + access_key + '&format=1';
 
 const options = {
     uri: link,
